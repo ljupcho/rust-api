85f4af7ddfefc84af3c1f20b2485618876474f75
Ljupcho <lui10e6@gmail.com>
2023-10-11 12:44:33 +0200

    build k8s rust task for migrations and db pool settings

--- src/shared/db.rs
+++ src/shared/db.rs
@@ -16,7 +16,7 @@ impl DB {
         let connection_string = format!("postgres://{0}:{1}@{2}:{3}/{4}", user, password, host, port, db_name);
 
         let mut opt: ConnectOptions = ConnectOptions::new(connection_string.to_string());
-        opt.max_connections(100)
+        opt.max_connections(25)
             .min_connections(25)
             .connect_timeout(Duration::from_secs(8))
             .idle_timeout(Duration::from_secs(8))
